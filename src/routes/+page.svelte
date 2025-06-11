ecd681ca3b7e186428382752d5daee02efb18173
Finley Baker <finjothegreat@gmail.com>
2025-06-11 07:02:10 -0600

    Add line about having a goal to unite animal rights and leftist movements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <p>
-	We are a grassroots movement committed to total liberation. Rooted in anti-oppression and
-	intersectionality, we fight for a world free from all systems of domination, including speciesism,
-	capitalism, racism, and ecocide. Through direct action, education, and community care, we work
-	toward collective freedom for all beings. Join us in building a just, compassionate future.
+	Our goal is to unite the animal rights and general leftist movements. We are a grassroots movement
+	committed to total liberation. Rooted in anti-oppression and intersectionality, we fight for a
+	world free from all systems of domination, including speciesism, capitalism, racism, and ecocide.
+	Through direct action, education, and community care, we work toward collective freedom for all
+	beings. Join us in building a just, compassionate future.
 </p>
