b9689a6ed9d9497da6953e23bafb8b9761f79ce1
Finley Baker <finjothegreat@gmail.com>
2025-06-13 07:48:42 -0600

    Add blurb about events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,13 @@
     and ecocide. Through direct action, education, and community care, we work toward collective
     freedom for all beings. Join us in building a just, compassionate future.
   </p>
+
+  <p>
+    We host a variety of events! See our <a
+      href="https://www.meetup.com/colorado-liberation-collective/">Meetup page</a
+    >
+    or our <a href="https://www.instagram.com/colorado.liberation.collective/">Instagram page</a> for
+    upcoming events. Past events have included letter-writing sessions for queer inmates, board game
+    nights, and reading clubs.
+  </p>
 </section>
