99cc76e1ede1847ccd1e090d1a42e9e59a781b95
Finley Baker <finjothegreat@gmail.com>
2025-06-15 08:35:12 -0600

    Add mailing list to page description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,10 @@
     We host a variety of events! See our <a
       href="https://www.meetup.com/colorado-liberation-collective/">Meetup page</a
     >
-    or our <a href="https://www.instagram.com/colorado.liberation.collective/">Instagram page</a> for
-    upcoming events. Past events have included letter-writing sessions for queer inmates, board game
-    nights, and reading clubs.
+    or our <a href="https://www.instagram.com/colorado.liberation.collective/">Instagram page</a>
+    for upcoming events. You can also
+    <a href="https://be739024f49211ef830505489aa2285c.eo.page/y943x">sign up to our newsletter</a>.
+    Past events have included letter-writing sessions for queer inmates, board game nights, and
+    reading clubs.
   </p>
 </section>
