cbebe668c8bb561412461f690e9a229e1df32a90
Finley Baker <finjothegreat@gmail.com>
2025-12-29 13:04:33 -0700

    Get rid of rotating subheader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,9 @@
   import '@picocss/pico';
 
   let { children } = $props();
-
-  const SUBHEADERS = [
-    'Humans don’t belong in cages, neither do animals',
-    'Love pigs, hate the police',
-    'Eat the rich, not the animals',
-  ];
-
-  const subheaderIndex = Math.floor(Math.random() * SUBHEADERS.length);
-  const subheader = SUBHEADERS[subheaderIndex];
 </script>
 
-<hgroup>
-  <h1>Colorado Liberation Collective</h1>
-  <p>{subheader}</p>
-</hgroup>
+<h1>Colorado Liberation Collective</h1>
 
 {@render children()}
 
